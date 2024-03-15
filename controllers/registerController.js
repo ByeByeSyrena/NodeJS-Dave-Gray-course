829251b643870be40045e751734a521c00d17a33
Ira S <iryna.shevchenko2@gmail.com>
2024-03-15 15:08:41 +0200

    rootes update

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,10 +1,9 @@
 const usersDB = {
   users: require("../model/users.json"),
-  setUser: function (data) {
+  setUsers: function (data) {
     this.users = data;
   },
 };
-
 const fsPromises = require("fs").promises;
 const path = require("path");
 const bcrypt = require("bcrypt");
@@ -15,16 +14,15 @@ const handleNewUser = async (req, res) => {
     return res
       .status(400)
       .json({ message: "Username and password are required." });
-
-  //check for duplicate in the db
+  // check for duplicate usernames in the db
   const duplicate = usersDB.users.find((person) => person.username === user);
-  if (duplicate) return res.sendStatus(409); // conflict
+  if (duplicate) return res.sendStatus(409); //Conflict
   try {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
     //store the new user
     const newUser = { username: user, password: hashedPwd };
-    usersDB.setUser([...usersDB.users, newUser]);
+    usersDB.setUsers([...usersDB.users, newUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "..", "model", "users.json"),
       JSON.stringify(usersDB.users)
--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
-const PORT = process.env.PORT || 3500;
 const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 3500;
 
-const path = require("path");
-
-//custom middleware for to listen to events
+// custom middleware logger
 app.use(logger);
 
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
-//built-in middleware to handle urlencoded data
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-//built-in middleware for json
+// built-in middleware for json
 app.use(express.json());
 
+//middleware for cookies
+app.use(cookieParser());
+
 //serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
+// routes
 app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
 
-app.use("/register", require("./routes/api/register"));
-app.use("/auth", require("./routes/api/auth"));
-
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
